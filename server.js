e3a1d53423d45842287597201aa69fe92b9656b4
Sakiskid <tylerdrew.home@gmail.com>
2020-12-14 15:13:51 -0600

    fix added comments and change / routing to * for react app html routing

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Define Middleware
 if(process.env.NODE_ENV === "production") {
     app.use(express.static('./google-books-search/build'));
 }
 
-app.get('/', (req, res) => {
+// Send every HTML route to React App
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './google-books-search/build/index.html'));
 });
 
+// Listen on env port
 app.listen(PORT, () => {
     console.log("Listening on port", PORT + "...");
 });
